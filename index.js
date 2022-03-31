f15ad5e0ce614f1280ca8decf48a9f72b446d019
Roger Zhang <1787009521@qq.com>
2022-03-31 10:18:56 +0800

    feat: 改造后

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 function myPromise() {
+    // this.__proto__ = myPromise.prototype
     this.handler = {}
 }
 // then函数就是将回调函数存放起来.
@@ -18,13 +19,14 @@ myPromise.prototype.then = function(onFuldilled, onRejected) {
 }
 
 
-function myDeferred() {
+function myDeferred(fn) {
     this.state = 'pending';
     this.promise = new myPromise()
+    fn(this.resolve.bind(this), this.reject.bind(this))
 }
 
 myDeferred.prototype.resolve = function(obj) {
-    console.log(this) // 这个this不等与上面的this // this是个变量，方法没有调用前我们都不确定this的指向，或者说你目前是按照你想向的方式去调用.
+    console.log(this.promise) // 这个this不等与上面的this // this是个变量，方法没有调用前我们都不确定this的指向，或者说你目前是按照你想向的方式去调用.
     this.state = 'fulfilled'
     var handler = this.promise.handler
     if(handler && handler.resolve) {
@@ -42,14 +44,17 @@ myDeferred.prototype.reject = function(obj) {
 
 // 异步代码
 function asyncDoSomething(flag, message) {
-    var deferred = new myDeferred()
-    setTimeout(function() {
-        if(flag) {
-            deferred.resolve({code: '200', message: message})
-        } else {
-            deferred.reject({code: '400', message: '拒绝'})
-        }
-    }, 3000)
+    var deferred = new myDeferred((resolve, reject) => {
+        setTimeout(function() {
+            if(flag) {
+                console.log(resolve.toString(), '11111')
+                resolve({code: '200', message: message})
+            } else {
+                reject({code: '400', message: '拒绝'})
+            }
+        }, 3000)
+    })
+
 
     return deferred.promise
 }
@@ -57,7 +62,7 @@ function asyncDoSomething(flag, message) {
 
 // asyncDoSomething(1, 'TD-King') > return deferred.promise 【这一部分代码返回myPromise对象】
 
-asyncDoSomething(1, 'TD-King').then((data) => {
+asyncDoSomething(0, 'TD-King').then((data) => {
     console.log(data)
 }, (err) => {
     console.log(err)
